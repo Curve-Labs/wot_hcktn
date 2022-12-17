6607c5190b084522aaedfac0eff6b98ce1179783
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-12-17 02:24:41 +0100

    adds comments

--- contracts/interfaces/ITrustAttestor.sol
+++ contracts/interfaces/ITrustAttestor.sol
@@ -2,9 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-
+/// @dev states functions that need to be implemented by a trust attestor
 interface ITrustAttestor {
-
     /// @dev attests that trust sigil with tokenId can be minted to recipient
     /// @param recipient recipient of trust sigil
     /// @param sender sender of trust sigil
