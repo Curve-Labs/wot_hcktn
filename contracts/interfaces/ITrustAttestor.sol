390605fd9c2a446b1c143fbb7fe8a38f479d8c4e
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-12-17 01:26:56 +0100

    adds comment

--- contracts/interfaces/ITrustAttestor.sol
+++ contracts/interfaces/ITrustAttestor.sol
@@ -8,5 +8,6 @@ interface ITrustAttestor {
     /// @dev attests that trust sigil with tokenId can be minted to recipient
     /// @param recipient recipient of trust sigil
     /// @param tokenId recipient of trust sigil
+    /// @param data encoded parameters that the respective attestor implementation requires for attesting
     function attest(address recipient, uint256 tokenId, bytes memory data) external returns (bool);
 }
