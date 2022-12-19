b81a507ecd09e671944d2e8af090c886144850ed
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-12-19 16:35:04 +0100

    adds tasks

---
+++ tasks/setup-sigil.js
@@ -0,0 +1,20 @@
+task("setup:sigil", "Deploys an instance of TrustSigil")
+  .addParam("id", "The base uri to be used for token metadata")
+  .addOptionalParam("attestor", "The address of the attestor contract")
+  .setAction(async ({ attestor, id }, { deployments, ethers }) => {
+    const ADDRESS_ONE = "0x1111111111111111111111111111111111111111";
+
+    const TrustSigil = await deployments.get("TrustSigil");
+    const trustSigil = await ethers.getContractAt(
+      "TrustSigil",
+      TrustSigil.address
+    );
+
+    let tx;
+
+    attestor
+      ? (tx = await trustSigil.setupSigil(id, attestor))
+      : (tx = await trustSigil.setupSigil(id, ADDRESS_ONE));
+
+    console.log("tx: ", tx);
+  });
